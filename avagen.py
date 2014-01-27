ad88e37eab5eb6b1f069cf95cc3973a0a5a4f795
Alexander <bellal89@mail.ru>
2014-01-27 22:26:59 +0600

    show method initiated

--- avagen.py
+++ avagen.py
@@ -1,19 +1,22 @@
 import sys, os
 import Image
 
-def show(image):
-	pass
+def show(pic_filename):
+	os.system(pic_filename)
 
 def process(image):
 	return image.resize((400, 400))
 
 
-for infile in sys.argv[1:]:
+# for infile in sys.argv[1:]:
+for infile in ["test.jpg"]:
     f, e = os.path.splitext(infile)
     outfile = f + "400x400.jpg"
     if infile != outfile:
         try:
             im = process(Image.open(infile))
+            im.save(outfile)
+            show(outfile)
         except IOError:
             print "cannot convert", infile
 
